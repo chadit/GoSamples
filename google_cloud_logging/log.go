f3b95f9951f203dacee678bf2975752e5f7f7c8d
Chad England <chadit@gmail.com>
2017-06-17 15:02:43 -0400

    .

--- google_cloud_logging/log.go
+++ google_cloud_logging/log.go
@@ -3,14 +3,40 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"google.golang.org/api/iterator"
-
+	//"go.uber.org/zap/zapcore"
 	"cloud.google.com/go/logging"
 	"cloud.google.com/go/logging/logadmin"
 )
 
+// follow these to setup
+// https://developers.google.com/identity/protocols/application-default-credentials
+// https://unix.stackexchange.com/questions/117467/how-to-permanently-set-environmental-variables
+const (
+	defaultEnvCredentialFilePath = "GOOGLE_APPLICATION_CREDENTIALS"
+	defaultEnvPrivateKey         = "GOOGLE_API_GO_PRIVATEKEY"
+	defaultEnvEmail              = "GOOGLE_API_GO_EMAIL"
+)
+
+var (
+	envCredential string
+	envEmail      string
+	envPrivateKey string
+)
+
+func init() {
+	envCredential = os.Getenv(defaultEnvCredentialFilePath)
+	envPrivateKey = os.Getenv(defaultEnvPrivateKey)
+	envEmail = os.Getenv(defaultEnvEmail)
+}
+
 func main() {
+	fmt.Println(envCredential)
+	fmt.Println(envPrivateKey)
+	fmt.Println(envEmail)
+
 	// Create a Client
 	ctx := context.Background()
 	client, err := logging.NewClient(ctx, "api-project-661531736098")
